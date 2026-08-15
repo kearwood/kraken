fe9a7ef487f1db24d0f09f44865fb3362951756a
Kearwood Gilbert <kearwood@kearwood.com>
2026-08-14 21:42:06 -0700

    Fix type-o

--- kraken/nodes/KRCamera.cpp
+++ kraken/nodes/KRCamera.cpp
@@ -486,7 +486,7 @@ void KRCamera::renderPost(RenderInfo& ri)
 void KRCamera::renderDebug(RenderInfo& ri)
 {
 
-  settings.m_debug_text = std::format("Frame Number {}", (getContext().getCurrentFrame()); // FNIDME!! KIPG!! HACK!! Debug text debugging...
+  settings.m_debug_text = std::format("Frame Number {}", getContext().getCurrentFrame()); // FNIDME!! KIPG!! HACK!! Debug text debugging...
 
   const char* szText = settings.m_debug_text.c_str();
   if (*szText == '\0') {
